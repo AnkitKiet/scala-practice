c347c465bdbe2bc6698db8bd9b9fad529b0a2dfa
Ankit Maurya <ankit.1410025@kiet.edu>
2019-03-17 15:50:00 +0530

    Init

---
+++ src/First.scala
@@ -0,0 +1,19 @@
+/*
+* First App to demonstrate how Scala works
+*
+* There are two ways to define main() method
+*/
+
+object First {
+
+  /*
+   def main(args: Array[String]) {
+    println("Hello");
+  }
+   */
+
+  def main(args: Array[String]): Unit = {
+    println("Hello");
+  }
+
+}
